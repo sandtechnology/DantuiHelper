8e1523a40b98a25ed74789ebd9e65e5def5e5396
sandtechnology <a1294790523@hotmail.com>
2020-04-11 02:12:14 +0800

    添加额外的项目信息

--- src/main/java/sandtechnology/utils/DataContainer.java
+++ src/main/java/sandtechnology/utils/DataContainer.java
@@ -13,7 +13,11 @@ public class DataContainer {
     private static final List<Long> targetGroup = new ArrayList<>();
     private static final List<Long> rukiTargetGroup = new ArrayList<>();
     private static final String version = "v2.2";
-    private static final WriteOnlyMessage message = new WriteOnlyMessage("机器人信息：").newLine().add("编写者：sandtechnology").newLine().add("版本号：").add(version).add(CQ == null ? "（Mirai内核）" : "（JCQ内核）").newLine().add("开源地址（基于AGPLv3开源）：https://github.com/sandtechnology/DantuiHelper");
+    private static final WriteOnlyMessage message = new WriteOnlyMessage("机器人信息：").newLine().add("编写者：sandtechnology").newLine().add("版本号：").add(version).add(isJCQ() ? "（JCQ内核）" : "（Mirai内核）").newLine().add("开源地址（基于AGPLv3开源）：https://github.com/sandtechnology/DantuiHelper").newLine().add(isJCQ() ? "JCQ项目地址：https://github.com/Meowya/JCQ-CoolQ" : "Mirai项目地址：https://github.com/mamoe/mirai");
+
+    public static boolean isJCQ() {
+        return CQ != null;
+    }
 
     public static String getVersion() {
         return version;
