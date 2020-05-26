82adc8c55124bd8d264d967fdf4ede474cc65d38
sandtechnology <a1294790523@hotmail.com>
2020-05-26 18:14:12 +0800

    添加测试

--- src/test/java/EmojiFormatTest.java
+++ src/test/java/EmojiFormatTest.java
@@ -17,8 +17,9 @@ public class EmojiFormatTest {
             String testMessage2 = "[奋斗]";
             emojis.add(new Emoji("[奋斗]", "https://i0.hdslb.com/bfs/emote/bb2060c15dba7d3fd731c35079d1617f1afe3376.png"));
             emojis.add(new Emoji("[妙啊]", "https://i0.hdslb.com/bfs/emote/b4cb77159d58614a9b787b91b1cd22a81f383535.png"));
-            System.out.println(new EmojiChain(emojis).format(new WriteOnlyMessage(textMessage)).toCQString());
-            System.out.println(new EmojiChain(emojis).format(new WriteOnlyMessage(testMessage2)).toCQString());
+            assert new EmojiChain(emojis).format(new WriteOnlyMessage(textMessage)).toCQString().equals("[CQ:image,file=0.hdslb.com\\bfs\\emote\\bb2060c15dba7d3fd731c35079d1617f1afe3376.png@50w_50h_1e_1c.png]瞅了一下，多半都是大会员专享跟大会员抢先看[CQ:image,file=0.hdslb.com\\bfs\\emote\\b4cb77159d58614a9b787b91b1cd22a81f383535.png@50w_50h_1e_1c.png]\n" +
+                    "那~就[CQ:emoji,id=128073]https:\\\\www.bilibili.com\\blackboard\\activity-_A80QNbtP.html?from=dongtai每天三毛钱，快乐你懂的[CQ:image,file=0.hdslb.com\\bfs\\emote\\bb2060c15dba7d3fd731c35079d1617f1afe3376.png@50w_50h_1e_1c.png]");
+            assert new EmojiChain(emojis).format(new WriteOnlyMessage(testMessage2)).toCQString().equals("[CQ:image,file=0.hdslb.com\\bfs\\emote\\bb2060c15dba7d3fd731c35079d1617f1afe3376.png@50w_50h_1e_1c.png]");
 
         }
     }
