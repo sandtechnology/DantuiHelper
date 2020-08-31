2a509b60c1f2ca57ac6a4f76fa3b90ec6caa20d2
sandtechnology <a1294790523@hotmail.com>
2020-08-31 22:41:35 +0800

    添加关于动态解析接口的文档

--- src/main/java/sandtechnology/bilibili/response/dynamic/dynamicCardAdapter/IAdapter.java
+++ src/main/java/sandtechnology/bilibili/response/dynamic/dynamicCardAdapter/IAdapter.java
@@ -2,14 +2,30 @@ package sandtechnology.bilibili.response.dynamic.dynamicCardAdapter;
 
 import sandtechnology.holder.WriteOnlyMessage;
 
+
 public interface IAdapter {
 
+    /**
+     * 获取解析结果的便利方法
+     *
+     * @return 包含解析结果的 WriteOnlyMessage
+     */
     default WriteOnlyMessage getContent() {
         return getContent(new WriteOnlyMessage());
     }
 
+    /**
+     * 获取解析结果 结果将直接显示给用户
+     *
+     * @param message 传入的 WriteOnlyMessage
+     * @return 加上解析结果后的 WriteOnlyMessage
+     */
     WriteOnlyMessage getContent(WriteOnlyMessage message);
 
+    /***
+     * 在B站未提供文本下的默认文本
+     * @return 动作文本 如 “投稿了视频”
+     */
     String getActionText();
 }
 
--- src/main/java/sandtechnology/bilibili/response/dynamic/dynamicCardAdapter/IRepostAdapter.java
+++ src/main/java/sandtechnology/bilibili/response/dynamic/dynamicCardAdapter/IRepostAdapter.java
@@ -11,5 +11,10 @@ public interface IRepostAdapter extends IAdapter {
         throw new UnsupportedOperationException("RepostAdapter Must Input DynamicData");
     }
 
+    /**
+     * 是否是分享动态
+     *
+     * @return 是否是分享动态，如分享番剧或直播间
+     */
     boolean isShareDynamic();
 }
